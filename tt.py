9141fe479d3f6f3aae8d36e8da8511f1746ca0eb
koo5 <kolman.jindrich@gmail.com>
2019-11-20 03:30:45 +0100

    ...icon...

--- tt.py
+++ tt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+import os
 import sys
 import psycopg2
 from collections import defaultdict, namedtuple
@@ -18,6 +18,7 @@ def store(action, misc):
 	with make_conn() as conn:
 		with conn.cursor() as curs:
 			curs.execute(sql, (action, misc))
+	print('db updated.')
 
 def report0():
 	sql = """SELECT * FROM hours ORDER BY ts;"""
@@ -86,6 +87,31 @@ def dump2():
 #def report3():
 #	runs = defaultdict(list)
 
+def noncritical_call(args):
+	try:
+		subprocess.check_call(args, timeout=5)
+	except Exception as err:
+		print("error: {0}".format(err))
+
+def notify(text):
+	ico = os.path.abspath(os.path.dirname(os.path.realpath(__file__))+'/ufo_by_telepatx_d2ne2p4-fullview2.ico')
+	noncritical_call(['notify-send', '-i', ico, 'unixy_time_tracker', text])
+
+def notify_running_change(on, arg):
+	print (arg)
+	start = arg == 'on'
+	stop = not start
+	
+	if start and not on:
+		msg = 'start'
+	elif start and on:
+		msg = 'already on'
+	elif stop and not on:
+		msg = 'already off'
+	elif stop and on:
+		msg = 'stop'
+	notify(msg)
+
 arg = sys.argv[1]
 if arg in ['on', 'off', 'desc']:
 	misc = ''
@@ -93,13 +119,14 @@ if arg in ['on', 'off', 'desc']:
 		misc = sys.argv[2]
 	if arg == 'on' and len(sys.argv) > 2:
 		store('desc', sys.argv[2])
+	old = dump2()
 	store(arg, misc)
 	if arg in ('on','off'):
-		subprocess.check_call(['notify-send', 'unixy_time_tracker', arg])
+		notify_running_change(old, arg)
 	if arg == 'on':
-		subprocess.check_call(['/home/koom/unixy_time_tracker/tt_beep'])
+		noncritical_call(['/home/koom/unixy_time_tracker/tt_beep'])
 elif arg == 'dump0':
-	for r in records():
+	for r in report0():
 		print(r)
 elif arg == 'dump1':
 	for i in report1()[0]:
@@ -118,5 +145,17 @@ elif arg == 'is_on':
 	else:
 		print('no')
 		exit(1)
+elif arg == 'is_on':
+	if report2()[1]:
+		print('yep')
+		exit(0)
+	else:
+		print('no')
+		exit(1)
+elif arg == 'tick':
+	if report2()[1]:
+		print('yep')
+		store('tick', '')
+		
 else:
 	raise('unknown command')
--- tt_stop_if_inactive.py
+++ tt_stop_if_inactive.py
@@ -4,5 +4,6 @@ import subprocess
 
 if int(subprocess.check_output(['xprintidle'])) > 10 * 60 * 1000:
 	if subprocess.call(['tt_is_on']) == 0:
+		print('idle, turning tt off')
 		subprocess.check_call(['tt', 'off'])
 
