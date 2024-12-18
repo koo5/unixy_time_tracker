c93f931c139374e8b36da5612fa319ac8de963c7
Jindrich Kolman <kolman.jindrich@gmail.com>
2024-12-18 07:43:10 +0100

    ..

---
+++ tt_tick.sh
@@ -0,0 +1,6 @@
+#!/bin/fish
+
+set DIR (dirname (readlink -m (status --current-filename))); cd "$DIR"
+
+./tt is_on && (./tt_beep && ./tt tick && ./notify.sh -t 3000 "tt tick" `date`) 2>&1 | logger -t tt
+
