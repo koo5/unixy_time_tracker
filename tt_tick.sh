8c1329e7cc56bf2e3b81c399e8f890ddb7f9fbc9
Your Name <koom@c1>
2025-01-23 15:43:15 +0100

    ..

--- tt_tick.sh
+++ tt_tick.sh
@@ -2,5 +2,5 @@
 
 set DIR (dirname (readlink -m (status --current-filename))); cd "$DIR"
 
-./tt is_on && ./tt tick && ./notify.sh -t 3000 "tt tick" `date` 2>&1 | logger -t tt
+./tt is_on && ./tt tick && ./notify.sh -t 3000 "tt tick" (date) 2>&1 | logger -t tt
 
